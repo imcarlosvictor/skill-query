4d7b4b95cc84ab7b0ec31baa172b5ac4a6b7c25d
Lucas <imcvlucas@gmail.com>
2023-06-23 22:42:35 -0400

    first commit

---
+++ src/bot_scraper.py
@@ -0,0 +1,18 @@
+class BotScraper:
+    def __init__(self):
+        pass
+
+    def get_hard_skills(self):
+        pass
+        
+    def get_soft_skills(self):
+        pass
+
+    def get_education(self):
+        pass
+
+    def get_location(self):
+        pass
+
+    def get_date(self):
+        pass
