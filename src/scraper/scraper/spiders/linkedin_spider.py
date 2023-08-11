016291571d3be86f5160d45f283dc04bc756c0ca
Lucas <imcvlucas@gmail.com>
2023-08-10 22:41:39 -0400

    feat: scrapy now crawls links for job posts

--- src/scraper/scraper/spiders/linkedin_spider.py
+++ src/scraper/scraper/spiders/linkedin_spider.py
@@ -3,14 +3,15 @@ import scrapy
 
 class LinkedinKeywordSpider(scrapy.Spider):
     name = 'linkedin_posts'
-    api_url = 'https://ca.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/software-engineer-jobs-toronto-on?start=25'
-    api_url_1 = 'https://ca.linkedin.com/jobs/search?keywords=Software+Developer&location=Toronto%2C+Ontario%2C+Canada&geoId=100761630&trk=public_jobs_jobs-search-bar_search-submit'
+    # url = ''
+    # api_url = 'https://ca.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/software-engineer-jobs-toronto-on?start=25'
+    url = 'https://ca.linkedin.com/jobs/search?keywords=junior+software+devloper&location=mississauga,on&geoId=100761630&trk=public_jobs_jobs-search-bar_search-submit'
 
 
     def start_requests(self):
         first_job_on_page = 0
-        first_url = self.api_url + str(first_job_on_page)
-        yield scrapy.Request(url=first_url, callback=self.parse_keywords, meta={'first_job_on_page': first_job_on_page})
+        url = self.url + str(first_job_on_page)
+        yield scrapy.Request(url=url, callback=self.parse_keywords, meta={'first_job_on_page': first_job_on_page})
 
     def parse_keywords(self, response):
         jobs = response.css('li') # Job postings
@@ -22,9 +23,10 @@ class LinkedinKeywordSpider(scrapy.Spider):
 
         job_item = {}
         for job in jobs:
-            job_item['company'] = job.css('a::text').get(default='n/a').strip()
-            # job_item['company'] = job.css('hidden-nested-link').get(default='n/a').strip()
-            job_item['title'] = job.css('base-search-card__title').get(default='n/a').strip()
-            job_item['location'] = job.css('job-search-card__location').get(default='n/a').strip()
-            job_item['date_posted'] = job.css('time::text').get(default='n/a').strip()
+            job_item['link'] = job.css('a::attr(href)').get(default='n/a')
+            # # job_item['company'] = job.css('a::text').get(default='n/a').strip()
+            # job_item['company'] = job.css('a::text').get(default='n/a').strip()
+            # job_item['title'] = job.css('base-search-card__title').get(default='n/a').strip()
+            # job_item['location'] = job.css('job-search-card__location').get(default='n/a').strip()
+            # job_item['date_posted'] = job.css('time::text').get(default='n/a').strip()
             yield job_item
