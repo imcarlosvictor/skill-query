1783e86ade615e0e5197b72fc3f071eca9400b6b
Lucas <imcvlucas@gmail.com>
2023-09-21 02:21:52 -0400

    Remove unused code

--- src/dashboard.py
+++ src/dashboard.py
@@ -12,8 +12,10 @@ from scrapy.crawler import CrawlerProcess
 from datetime import datetime
 
 # from scrapy_spiders import spider_control 
-from scrapy_spiders.scrapy_spiders.spiders.software_eng_spider import SoftwareEngineerSpider, SoftwareEngineerPostSpider
-from scrapy_spiders.scrapy_spiders.spiders.data_analyst_spider import DataAnalystSpider, DataAnalystPostSpider
+from scrapy_spiders.scrapy_spiders.spiders.software_eng_spider import SoftwareEngineerSpider
+from scrapy_spiders.scrapy_spiders.spiders.data_analyst_spider import DataAnalystSpider
+from scrapy_spiders.scrapy_spiders.spiders.swe_post_spider import SoftwareEngineerPostSpider
+from scrapy_spiders.scrapy_spiders.spiders.da_post_spider import DataAnalystPostSpider
 
 
 FILENAME = __file__
@@ -37,20 +39,26 @@ class Dashboard:
             ('Software Engineer', 'Data Analyst')
         )
 
-        location_option = st.sidebar.selectbox(
-            'Location',
-            ('Canada', 'USA')
-        )
+        # location_option = st.sidebar.selectbox(
+        #     'Location',
+        #     ('Canada', 'USA')
+        # )
 
         year_option = st.sidebar.selectbox(
             'Year',
             ('2023',)
         )
 
-        show_btn = st.sidebar.button('Show', use_container_width=True)
+        # show_btn = st.sidebar.button('Show', use_container_width=True)
 
 
         # ############ Dashboard ############
+        role = ''
+        if job_role_option == 'Software Engineer':
+            role = 'software_engineer'
+        elif job_role_option == 'Data Analyst':
+            role = 'data_analyst'
+
         top_container = st.container()
         with top_container:
             self.plot_map()
@@ -58,28 +66,20 @@ class Dashboard:
         mid_container = st.container()
         with mid_container:
             col3, col4 = st.columns([3,4], gap='large')
-            self.plot_technology_graph(col3)
-            self.plot_framework_graph(col4)
+            self.plot_technology_graph(col3, role)
+            self.plot_framework_graph(col4, role)
 
         bottom_container = st.container()
         with bottom_container:
             col5, col6 = st.columns([5,6],gap='large')
             # self.plot_experience_graph(col5)
-            self.plot_education_graph(col6)
+            self.plot_education_graph(col6, role)
 
-
-        ############ Call Spider ############
-        if show_btn:
-            # run_spider.start_spider()
-            # st.write('Why hello there')
-            print('hello there')
-            # pass
-        #####################################
 
     def plot_map(self):
         st.map()
 
-    def plot_technology_graph(self, col):
+    def plot_technology_graph(self, col, job_role):
         year, month = self.get_date()
         # Load data
         with open(DASH_DATA_TARGET_FILE, 'r') as jsonFile:
@@ -87,8 +87,8 @@ class Dashboard:
         # plotly chart
         with col:
             col.header('Technologies')
-            y_label = [key for key in data[year][month]['technology'].keys()]
-            x_label = [val for val in data[year][month]['technology'].values()]
+            y_label = [key for key in data[job_role][year][month]['technology'].keys()]
+            x_label = [val for val in data[job_role][year][month]['technology'].values()]
             fig = go.Figure(go.Bar(
                 x=x_label,
                 y=y_label,
@@ -100,10 +100,10 @@ class Dashboard:
             fig.update_xaxes(visible=False)
             fig.update_yaxes(categoryorder='total ascending')
             fig.update_layout(height=600,margin={'t':0,'b':.7})
-            fig.update_traces(marker_color='#ff1745')
+            fig.update_traces(marker_color='#ff0044')
             st.plotly_chart(fig, theme='streamlit', use_container_width=True)
 
-    def plot_framework_graph(self, col):
+    def plot_framework_graph(self, col, job_role):
         year, month = self.get_date()
         # load data
         with open(DASH_DATA_TARGET_FILE, 'r') as jsonFile:
@@ -111,8 +111,8 @@ class Dashboard:
         # plotly chart
         with col:
             col.header('Frameworks')
-            y_label = [y_key for y_key in data[year][month]['frameworks'].keys()]
-            x_label = [x_val for x_val in data[year][month]['frameworks'].values()]
+            y_label = [y_key for y_key in data[job_role][year][month]['frameworks'].keys()]
+            x_label = [x_val for x_val in data[job_role][year][month]['frameworks'].values()]
             fig = go.Figure(go.Bar(
                 x=x_label,
                 y=y_label,
@@ -124,10 +124,11 @@ class Dashboard:
             fig.update_xaxes(visible=False)
             fig.update_yaxes(categoryorder='total ascending')
             fig.update_layout(height=600,margin={'t':0})
-            fig.update_traces(marker_color='#ff1745')
+            fig.update_traces(marker_color='#ff0044')
             st.plotly_chart(fig, theme='streamlit', use_container_width=True)
 
-    def plot_education_graph(self, col):
+    # TODO:
+    def plot_education_graph(self, col, job_role):
         year, month = self.get_date()
         # load data
         with open(DASH_DATA_TARGET_FILE, 'r') as jsonFile:
@@ -136,14 +137,15 @@ class Dashboard:
         with col:
             col.header('Education')
 
-            names = [keys for keys in data[year][month]['education'].keys()]
-            values = [values for values in data[year][month]['education'].values()]
+            names = [keys for keys in data[job_role][year][month]['education'].keys()]
+            values = [values for values in data[job_role][year][month]['education'].values()]
             fig = px.pie(values=values, names=names, color_discrete_sequence=px.colors.sequential.RdBu)
             fig.update_layout(legend_title=False)
             fig.update_traces(textposition='inside', textinfo='percent+label', showlegend=False)
             st.plotly_chart(fig, theme='streamlit', use_container_width=True)
 
-    def plot_experience_graph(self, col):
+    # TODO:
+    def plot_experience_graph(self, col, job_role):
         year, month = self.get_date()
         # load data
         with open(DASH_DATA_TARGET_FILE, 'r') as jsonFile:
@@ -151,8 +153,8 @@ class Dashboard:
         # plotly
         with col:
             col.header('Experience')
-            y_key = [y_key for y_key in data[year][month]['experience'].keys()]
-            x_val = [x_val for x_val in data[year][month]['experience'].values()]
+            y_key = [y_key for y_key in data[job_role][year][month]['experience'].keys()]
+            x_val = [x_val for x_val in data[job_role][year][month]['experience'].values()]
             fig = go.Figure(go.Bar(
                 # x=x_label,
                 y=y_label,
@@ -164,7 +166,7 @@ class Dashboard:
             fig.update_xaxes(visible=False)
             fig.update_yaxes(categoryorder='total ascending')
             fig.update_layout(height=600,margin={'t':0})
-            fig.update_traces(marker_color='#ff1745')
+            fig.update_traces(marker_color='#ff0044')
             st.plotly_chart(fig, theme='streamlit', use_container_width=True)
 
     def get_date(self):
